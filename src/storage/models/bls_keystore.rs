29cbb78f8a1ae804c2acc6dc6fefdcd2084dea27
ghostant-1017 <syyanyuhui@qq.com>
2023-08-29 10:35:48 +0800

    Impl query keystore

--- src/storage/models/bls_keystore.rs
+++ src/storage/models/bls_keystore.rs
@@ -1,11 +1,35 @@
 use super::*;
+use anyhow::ensure;
+use bb8_postgres::tokio_postgres::Row;
 use eth2_keystore::Keystore;
 
-pub async fn query_unused_key_store(conn: &mut PgConnection<'_>) -> Result<Keystore> {
-    let row = conn
-        .query_one("select * from bls_addresses limit 1;", &[])
+pub struct StoredKeyStore {
+    pub pk: i64,
+    pub key_store: Keystore,
+    pub event_pk: Option<i64>
+}
+
+impl TryFrom<Row> for StoredKeyStore {
+    type Error = anyhow::Error;
+
+    fn try_from(row: Row) -> std::result::Result<Self, Self::Error> {
+        let pk: i64 = row.try_get("pk")?;
+        let data: serde_json::Value = row.try_get("key_store")?;
+        let key_store = Keystore::from_json_str(&data.to_string()).map_err(|_| anyhow!("serde error"))?;
+        let event_pk: Option<i64> = row.try_get("event_pk")?;
+        Ok(StoredKeyStore { pk, key_store, event_pk })
+    }
+}
+
+pub async fn query_unused_key_store(conn: &mut PgConnection<'_>, n: i64) -> Result<Vec<StoredKeyStore>> {
+    let rows = conn
+        .query("select * from bls_addresses where event_pk is null limit {};", &[&n])
         .await?;
-    let data: serde_json::Value = row.get("key_store");
-    // let ks: JsonKeystore= serde_json::from_str(&data)?;
-    Keystore::from_json_str(&data.to_string()).map_err(|_| anyhow!("serde error"))
+    let mut result = vec![];
+    for row in rows {
+        let ks = StoredKeyStore::try_from(row)?;
+        result.push(ks);
+    }
+    ensure!(result.len() == n as usize, "Not enough bls keystore, expect: {}, found: {}.", n, result.len());
+    return Ok(result)
 }
