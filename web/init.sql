7c81dd75221e54f96a863b405f3c236d2772aee0
ghostant-1017 <syyanyuhui@qq.com>
2023-09-18 16:51:35 +0800

    Fix sql create table

--- web/init.sql
+++ web/init.sql
@@ -23,7 +23,8 @@ CREATE INDEX validators_wc_idx ON hellman_validators(withdrawal_credentials);
 CREATE TABLE exit_messages(
     pk BIGSERIAL PRIMARY KEY,
     validator_index BIGINT UNIQUE NOT NULL,
-    raw_message VARCHAR NOT NULL,
-    signature VARCHAR NOT NULL,
+    user_message VARCHAR NOT NULL,
+    user_signature VARCHAR NOT NULL,
+    signed_voluntary_exit JSON  NOT NULL,
     created_time timestamptz default now()
 )
